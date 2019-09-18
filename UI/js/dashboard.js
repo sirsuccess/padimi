b08ef37bcf8ad8e39f59c7e114745a504cc81327
ugwumadu116 <ugwumadu116@gmail.com>
2019-09-19 00:14:32 +0100

    update user info

--- UI/js/dashboard.js
+++ UI/js/dashboard.js
@@ -5,79 +5,40 @@ const settingsDetailsDOM = document.querySelector(".settings-details");
 
 const settingInfo = [
   `<div class="card card-fluid">
-  <h6 class="card-header"> Public Profile </h6><!-- .card-body -->
+  <h6 class="card-header"> Public Profile </h6>
   <div class="card-body">
-    <!-- .media -->
     <div class="media mb-3">
-      <!-- avatar -->
       <div class="user-avatar user-avatar-xl fileinput-button">
         <div class="fileinput-button-label change-photo-img"> Change photo </div>
-        <img src="./img/33.jpg" alt=""> <input id="fileupload-avatar" type="file" name="avatar">
-      </div><!-- /avatar -->
-      <!-- .media-body -->
+        <img src="./img/33.jpg" alt=""> <input id="fileupload-avatar" class="change-photo" type="file" name="avatar">
+      </div>
       <div class="media-body pl-3 ml-5">
         <h3 class="card-title"> Public avatar </h3>
         <h6 class="card-subtitle text-muted"> Click the current avatar to change your photo. </h6>
         <p class="card-text">
           <small>JPG, GIF or PNG 400x400, &lt; 2 MB.</small>
-        </p><!-- The avatar upload progress bar -->
+        </p>
         <div id="progress-avatar" class="progress progress-xs fade">
           <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
-        </div><!-- /avatar upload progress bar -->
-      </div><!-- /.media-body -->
-    </div><!-- /.media -->
-    <!-- form -->
+        </div>
+      </div>
+    </div>
     <form method="post">
-      <!-- form row -->
       <div class="form-row">
-        <!-- form column -->
-        <label for="input01" class="col-md-3">Cover image</label> <!-- /form column -->
-        <!-- form column -->
+        <label for="input01" class="col-md-3">Profile image</label> 
         <div class="col-md-9 mb-3">
           <div class="custom-file">
-            <input type="file" class="custom-file-input" id="input01" multiple=""> <label class="custom-file-label" for="input01">Choose cover</label>
+            <input type="file" class="custom-file-input" id="input01" multiple=""> <label class="custom-file-label" for="input01">Choose profile image</label>
           </div><small class="text-muted">Upload a new cover image, JPG 1200x300</small>
-        </div><!-- /form column -->
-      </div><!-- /form row -->
-      <!-- form row -->
-      <!-- <div class="form-row"> -->
-        <!-- form column -->
-        <!-- <label for="input02" class="col-md-3">Company</label>  -->
-        <!-- /form column -->
-        <!-- form column -->
-        <!-- <div class="col-md-9 mb-3"> -->
-          <!-- <input type="text" class="form-control" id="input02" value="CreativeDivision"> -->
-        <!-- </div> -->
-        <!-- /form column -->
-      <!-- </div> -->
-      <!-- /form row -->
-      <!-- form row -->
-      <div class="form-row">
-        <!-- form column -->
-        <label for="input03" class="col-md-3">Profile Heading</label> <!-- /form column -->
-        <!-- form column -->
-        <div class="col-md-9 mb-3">
-          <textarea class="form-control" id="input03">Huge fan of PDP.</textarea> <small class="text-muted">Appears on your profile page, 300 chars max.</small>
-        </div><!-- /form column -->
-      </div><!-- /form row -->
-      <!-- form row -->
-      <div class="form-row">
-        <!-- form column -->
-        <label for="input04" class="col-md-3">Want to be shadow?</label> <!-- /form column -->
-        <!-- form column -->
-        <div class="col-md-9 mb-3">
-          <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="input04" checked=""> <label class="custom-control-label" for="input04">Yes, choose me</label>
-          </div>
-        </div><!-- /form column -->
-      </div><!-- /form row -->
+        </div>
+      </div>
+      
       <hr>
-      <!-- .form-actions -->
       <div class="form-actions">
         <button type="submit" class="btn btn-primary ml-auto">Update Profile</button>
-      </div><!-- /.form-actions -->
-    </form><!-- /form -->
-  </div><!-- /.card-body -->
+      </div>
+    </form>
+  </div>
 </div>`,
   `<!-- .card -->
   <div class="card card-fluid">
@@ -88,16 +49,6 @@ const settingInfo = [
 <form method="post">
 <!-- form row -->
 <div class="form-row">
-<!-- form column -->
-<div class="col-md-6 mb-3">
-<label for="input01">First Name</label>
-<input type="text" class="form-control" id="input01" value="Beni" required=""> </div>
-<!-- /form column -->
-<!-- form column -->
-<div class="col-md-6 mb-3">
-<label for="input02">Last Name</label>
-<input type="text" class="form-control" id="input02" value="Arisandi" required=""> </div>
-<!-- /form column -->
 </div>
 <!-- /form row -->
 <!-- .form-group -->
@@ -109,13 +60,6 @@ const settingInfo = [
 <div class="form-group">
 <label for="input04">New Password</label>
 <input type="password" class="form-control" id="input04" value="secret" required=""> </div>
-<!-- /.form-group -->
-<!-- .form-group -->
-<div class="form-group">
-<label for="input05">Username</label>
-<input type="text" class="form-control" id="input05" value="bent10" required="">
-<small class="text-muted">Dolores reiciendis, eos accusamus nobis at omnis consequuntur culpa tempore saepe animi.</small>
-</div>
 <!-- /.form-group -->
 <hr>
 <!-- .form-actions -->
